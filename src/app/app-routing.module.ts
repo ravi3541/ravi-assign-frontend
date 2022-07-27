662ebf82c2c34fd8dd64e6639d974f57fdbc92a5
Ravi Mourya <mouryaravi3541@gmail.com>
2022-07-27 22:45:24 +0530

    designed header and integrated stripe using form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ShoppingModule } from './modules/shopping/shopping.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"shopping", loadChildren:()=> import('./modules/shopping/shopping.module').then(m => m.ShoppingModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
